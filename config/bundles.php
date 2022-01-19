7061b534017e2434d0be42542b5a5bc64c3dc71c
Michael Best <mbest@treetime.ca>
2022-01-19 13:34:08 -0700

    - add phpunit and maker-bundle to add WebTestCases - change main > guard > authenticators to main > custom_authenticators - change main > guard > entry_point to main > entry_point - enable enable_authenticator_manager - put quotes on access control paths

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
